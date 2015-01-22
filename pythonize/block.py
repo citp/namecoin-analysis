02f8bc4b00297ac06179112c187734c3ed8d376c
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-01-22 17:33:37 -0500

    Add my crap.

---
+++ pythonize/block.py
@@ -0,0 +1,5 @@
+class Block():
+    def __init__(self, block_num, transactions, self_hash):
+        self.block_num = block_num
+        self.transactions = transactions
+        self.self_hash = self_hash
---
+++ pythonize/transaction.py
@@ -0,0 +1,3 @@
+class Transaction():
+    def __init__(self, transaction_json):
+        self._transaction_json = transaction_json
