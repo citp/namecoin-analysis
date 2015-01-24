dc2eadec1c03cf32b6a6296f3a7248549fbdee8e
Paul Ellenbogen <pe5@cs.princeton.edu>
2015-01-24 16:39:45 -0500

    Create script to scrape domains for sale.

---
+++ dotbit_domains/listing.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+
+class Listing():
+    def __init__(self, domain, prices, date_seen):
+        self.domain = domain
+        self.date_seen = date_seen
+        for coin_name, price in prices.items():
+            if price.find("-") < 0:
+                price = Decimal(price)
+            else:
+                price = None
+            setattr(self, coin_name, price)
+
+        
--- dotbit_domains/run.py
+++ dotbit_domains/run.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python3
 
-import requests
+# Python std lib
 import pdb
+from re import finditer, search
+from datetime import datetime
+from time import sleep
+from pickle import dump, HIGHEST_PROTOCOL
+
+# 3rd party
+import requests
+
+# My stuff
+from listing import Listing
+
 
 REQUEST_URL_PREFIX = "https://dotbit.me/get_domains_for_sale.php"
+# This regex looks ugly. Maybe fix it later.
+REGEX = """<td>([a-zA-Z0-9-_]+\.bit)</td><td class=sp onclick='buyPrivateDomain\("[a-zA-Z0-9-_]+","Bitcoins","[0-9.-]+"\);'>([0-9-.]+)</td><td onclick='buyPrivateDomain\("[a-zA-Z0-9-_]+","Namecoins","[0-9.-]+"\);' class=sp>([0-9-]+)</td><td onclick='buyPrivateDomain\("[a-zA-Z0-9-_]+","Litecoins","[0-9.-]+"\);' class=sp>([0-9.-]+)</td>\s+<td onclick='buyPrivateDomain\("[a-zA-Z0-9-_]+","Peercoins","[0-9.-]+"\);' class=sp>([0-9.-]+)</td>\s*<td onclick='buyPrivateDomain\("[a-zA-Z0-9-_]+","Primecoins","[0-9.-]+"\);' class=sp>([0-9.-]+)</td>"""
 
-domains = []
+listings = []
 i = 1
 while True:
-    params = {"p": 1}
+    if i % 10 == 0:
+        print("Now fetching page " + str(i))
+    params = {"p": i}
     r = requests.get(REQUEST_URL_PREFIX, params = params)
-    pdb.set_trace()
-    if r.text is None:
+    if r.text is None or search(REGEX, r.text) is None:
         break
-    print(r.text)
+    for match in finditer(REGEX, r.text):
+        domain = match.group(1)
+        bitcoin_price = match.group(2)
+        namecoin_price = match.group(3)
+        litecoin_price = match.group(4)
+        peercoin_price = match.group(5)
+        primecoin_price = match.group(6)
+        prices = {"bitcoin_price": bitcoin_price,
+                  "namecoin_price": namecoin_price,
+                  "litecoin_price": litecoin_price,
+                  "peercoin_price": peercoin_price,
+                  "primecoin_price": primecoin_price}
+        listings.append(Listing(domain, prices, datetime.now()))
     i += 1
+    sleep(0.5)
     
-
+filename = datetime.now().strftime("%Y-%m-%d-%H_%M.pickle")
+with open(filename, "wb") as output_file:
+    dump(listings, output_file, protocol = HIGHEST_PROTOCOL)
 
