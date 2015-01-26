f1f0707c83a739982fff3792cce59d17ba2c46fc
Harry Kalodner <harry.kalodner@gmail.com>
2015-01-26 17:03:12 -0500

    Updated common.py

--- common.py
+++ common.py
@@ -1,19 +1,52 @@
-import pickle
+#!/usr/bin/env python
 
-class NameInfo:
-    def __init__(self, height, tx_pos, txout_pos, tx_type, name, value):
+import cPickle as pickle
+import hashlib
+
+
+class TransactionOutput(object):
+    def __init__(self, height, tx_pos, txout_pos, from_pub_id, to_pub_id):
         self.height = height
         self.tx_pos = tx_pos
         self.txout_pos = txout_pos
-        self.tx_type = tx_type
+        self.from_pub_id = from_pub_id
+        self.to_pub_id = to_pub_id
+
+class NameNew(TransactionOutput):
+    def __init__(self, height, tx_pos, txout_pos, from_pub_id, to_pub_id, tx_hash):
+        super(NameNew, self).__init__(height, tx_pos, txout_pos, from_pub_id, to_pub_id)
+        self.tx_hash = tx_hash
+
+    def __str__(self):
+        return "NameNew({}, {}, {}, {})".format(self.height, self.tx_pos, self.from_pub_id, self.to_pub_id)
+
+    def tx_type(self):
+        return "new"
+
+class NameUpdate(TransactionOutput):
+    def __init__(self, height, tx_pos, txout_pos, from_pub_id, to_pub_id, name, value):
+        super(NameUpdate, self).__init__(height, tx_pos, txout_pos, from_pub_id, to_pub_id)
         self.name = name
         self.value = value
 
     def __str__(self):
-    	return "NameInfo({}, {}, {}, {})".format(self.height, self.tx_type, self.name, self.value)
+        return "NameUpdate({}, {}, {}, {}, {}, {})".format(self.height, self.tx_pos, self.from_pub_id, self.to_pub_id, self.name, self.value)
 
-    def __repr__(self):
-    	return "NameInfo({}, {}, {}, {})".format(self.height, self.tx_type, self.name, self.value)
+    def tx_type(self):
+        return "update"
+
+class NameFirstUpdate(NameUpdate):
+    def __init__(self, height, tx_pos, txout_pos, from_pub_id, to_pub_id, name, tx_rand, value):
+        super(NameFirstUpdate, self).__init__(height, tx_pos, txout_pos, from_pub_id, to_pub_id, name, value)
+        self.tx_rand = tx_rand
+
+    def __str__(self):
+        return "NameFirstUpdate({}, {}, {}, {}, {}, {})".format(self.height, self.tx_pos, self.from_pub_id, self.to_pub_id, self.name, self.value)
+
+    def tx_type(self):
+        return "firstUpdate"
+
+
 
 def load_object(filename):
     with open(filename, 'rb') as input:
@@ -22,3 +55,13 @@ def load_object(filename):
 def save_object(obj, filename):
     with open(filename, 'wb') as output:
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+def ripemd160(data):
+    return hashlib.new("ripemd160", data)
+
+def hash160(data):
+    """A standard compound hash."""
+    return ripemd160(hashlib.sha256(data).digest()).digest()
+
+# test = NameNew(13,153, 532, "fsdfsfds")
+# print test
