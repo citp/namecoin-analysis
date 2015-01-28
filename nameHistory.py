e80ba8807ebf34e0d5829e5bcbb96d52275a1072
Harry Kalodner <harry.kalodner@gmail.com>
2015-01-28 16:27:28 -0500

    Added bucket abstraction

--- nameHistory.py
+++ nameHistory.py
@@ -9,21 +9,40 @@ import json
 
 execfile("common.py")
 
-def getNumberOfChangeUpdates(opList):
+def getNumberOfNameTransactions(opList):
+    return len(opList)
+
+def getNumberOfValueChanges(opList):
     changeUpdates = 0
     lastValue = ""
     for op in opList:
-        if op.tx_type() == "new":
-            changeUpdates = 0
-        elif op.tx_type() == "firstUpdate":
+        if op.tx_type() == "firstUpdate":
             lastValue = op.value
         elif op.tx_type() == "update":
             if op.value != lastValue:
                 changeUpdates += 1
-                lastValue = op.value
+            lastValue = op.value
     return changeUpdates
 
+def getNumberOfOwnerChanges(opList):
+    ownerChanges = 0
+    lastOwner = ""
+    for op in opList:
+        if op.tx_type() != "new":
+            if op.to_pub_id == lastOwner:
+                ownerChanges += 1
+        lastOwner = op.to_pub_id
+    return ownerChanges
 
+def getCounts(nameDict, nameListFunc):
+    bucketList = {}
+    for name in nameDict:
+        bucket = nameListFunc(nameDict[name])
+        if bucket in bucketList:
+            bucketList[bucket] += 1
+        else:
+            bucketList[bucket] = 1
+    return bucketList
 
 def main(argv):
 
@@ -71,34 +90,21 @@ def main(argv):
         nameDict = load_object("nameDict.dat")
         # nameNewDict = load_object("unusedNameNew.dat")
 
-    updateCount = {}
-    changeUpdateCount = {}
+    updateCount = getCounts(nameDict, getNumberOfNameTransactions)
+    changeUpdateCount = getCounts(nameDict, getNumberOfValueChanges)
+    ownerChangeCount = getCounts(nameDict, getNumberOfOwnerChanges)
 
-    for name in nameDict:
-        length = len(nameDict[name])
-        changeUpdates = getNumberOfChangeUpdates(nameDict[name])
-
-        if length in updateCount:
-            updateCount[length] += 1
-        else:
-            updateCount[length] = 1
-
-        if changeUpdates > 8:
-            print name
-
-        if changeUpdates in changeUpdateCount:
-            changeUpdateCount[changeUpdates] += 1
-        else:
-            changeUpdateCount[changeUpdates] = 1
-
-    print "Updates"
+    print "Name Transactions"
     for length in updateCount:
         print length, updateCount[length]
 
-    print "Change Updates"
+    print "Value Changes"
     for length in changeUpdateCount:
         print length, changeUpdateCount[length]
 
+    print "Owner Changes"
+    for length in ownerChangeCount:
+        print length, ownerChangeCount[length]
     
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
